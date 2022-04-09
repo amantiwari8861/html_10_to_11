79ef3d5d637cce5453c49e544b4560d6b75fd6e9
Aman Tiwari <36691571+amantiwari8861@users.noreply.github.com>
2022-04-09 13:39:10 +0530

    first

---
+++ second.c
@@ -0,0 +1,16 @@
+#include<stdio.h> // header file standard input output header file
+#include<conio.h> // header file for getch()
+
+int main()  //starting point of a program in c
+{
+    printf("hello world"); // print hello world
+    getch(); // getch() is used to get a character from the keyboard
+    return 0; // ending point of a program in c
+}
+
+/*
+h 
+e 
+l
+l
+o */
